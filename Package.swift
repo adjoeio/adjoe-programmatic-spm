33886139482a36b2ccf5cc70af92bde1b3d5a370
Amir Mohamadi <amir.mohamadi@adjoe.io>
2026-07-08 20:00:00 +0200

    [Update] AdjoeWaveSDK (1.7.0)

--- Package.swift
+++ Package.swift
@@ -15,8 +15,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "AdjoeWaveSDK",
-            url: "https://releases.adjoe.io/wave/ios/1.6.0/AdjoeWaveSDK.zip",
-            checksum: "173731e0628a3b2a3d23845aca1427c5dd699f2444d059d2b1016ef6aa657e7b"
+            url: "https://releases.adjoe.io/wave/ios/1.7.0/AdjoeWaveSDK.zip",
+            checksum: "8ee6b4fac783888df6cf04e04c4405f0a56c2778f8bb59a89b7e0076dddcb954"
         )
     ]
 )
